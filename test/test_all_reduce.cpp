cd698b5c7cc5caaaea31d23ed32300a41b16b1ed
Jie Ren <jieren9806@gmail.com>
2024-05-03 01:56:46 +0300

    feat(test): init mpi thread pool test

--- test/test_all_reduce.cpp
+++ test/test_all_reduce.cpp
@@ -27,7 +27,7 @@ class AllReduceMPITestFixture : public ::testing::Test {
 
 namespace {
 template <typename T>
-void TestForwardT(const dllm::Context &context, int rank, MPI_Comm comm) {
+void TestAllReduceT(const dllm::Context &context, int rank, MPI_Comm comm) {
   const int m = 128;
   auto shapeX = cute::make_shape(m);
   auto layoutX = cute::make_layout(shapeX, cute::GenRowMajor{});
@@ -64,8 +64,83 @@ void TestForwardT(const dllm::Context &context, int rank, MPI_Comm comm) {
 }  // namespace
 
 TEST_F(AllReduceMPITestFixture, TestForwardF32) {
-  TestForwardT<float>(context, localRank, comm);
+  TestAllReduceT<float>(context, localRank, comm);
 }
 TEST_F(AllReduceMPITestFixture, TestForwardF64) {
-  TestForwardT<double>(context, localRank, comm);
+  TestAllReduceT<double>(context, localRank, comm);
+}
+
+class AllReduceMPIThreadPoolTestFixture : public ::testing::Test {
+ protected:
+  int rank{[] {
+    int rank;
+    CHECK_MPI(MPI_Comm_rank(MPI_COMM_WORLD, &rank));
+    return rank;
+  }()};
+  int worldSize{[] {
+    int worldSize;
+    CHECK_MPI(MPI_Comm_size(MPI_COMM_WORLD, &worldSize));
+    return worldSize;
+  }()};
+  MPI_Comm comm{[] {
+    MPI_Comm comm;
+    MPI_Comm_split_type(MPI_COMM_WORLD, MPI_COMM_TYPE_SHARED, 0, MPI_INFO_NULL,
+                        &comm);
+    return comm;
+  }()};
+  int localRank{[&] {
+    int localRank;
+    MPI_Comm_rank(comm, &localRank);
+    return localRank;
+  }()};
+  dllm::ThreadPool threadPool{localRank, 3};
+
+  ~AllReduceMPIThreadPoolTestFixture() { MPI_Comm_free(&comm); }
+};
+
+namespace {
+template <typename T>
+void TestThreadPoolAllReduceT(dllm::ThreadPool &threadPool, int rank,
+                              MPI_Comm comm) {
+  const int m = 128;
+  auto shapeX = cute::make_shape(m);
+  auto layoutX = cute::make_layout(shapeX, cute::GenRowMajor{});
+
+  Eigen::Vector<T, Eigen::Dynamic> x(m);
+
+  auto tensorX = std::make_shared<dllm::DTensor1D<dllm::communication::MPI>>(
+      rank, comm,
+      dllm::Tensor1D{x.data(), layoutX, dllm::toDtype<T>(), dllm::CUDA});
+
+  std::srand(rank + 1);
+  x.setRandom();
+
+  auto task =
+      dllm::communication::AllReduce<dllm::communication::MPI>::runInplace(
+          tensorX, dllm::communication::SUM);
+  tensorX.reset();
+  auto future = threadPool.submit(std::move(task));
+  future->wait();
+
+  Eigen::Vector<T, Eigen::Dynamic> accumulator(m);
+  if (rank == 0) {
+    int worldSize;
+    CHECK_MPI(MPI_Comm_size(comm, &worldSize));
+    accumulator.setZero();
+    for (int i = 0; i < worldSize; ++i) {
+      std::srand(i + 1);
+      accumulator += Eigen::Vector<T, Eigen::Dynamic>(m).setRandom();
+    }
+    ASSERT_NEAR(
+        accumulator.array().abs().maxCoeff() - x.array().abs().maxCoeff(), 0,
+        1e-4);
+  }
+}
+}  // namespace
+
+TEST_F(AllReduceMPIThreadPoolTestFixture, TestForwardF32) {
+  TestThreadPoolAllReduceT<float>(threadPool, localRank, comm);
+}
+TEST_F(AllReduceMPIThreadPoolTestFixture, TestForwardF64) {
+  TestThreadPoolAllReduceT<double>(threadPool, localRank, comm);
 }
