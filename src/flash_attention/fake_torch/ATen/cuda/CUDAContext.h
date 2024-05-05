263240390d6edcd3a399209eb71e26bc06c4c021
Jie Ren <jieren9806@gmail.com>
2024-05-05 23:59:33 +0300

    feat(flash_attention): add attn, layer_norm

--- src/flash_attention/fake_torch/ATen/cuda/CUDAContext.h
+++ src/flash_attention/fake_torch/ATen/cuda/CUDAContext.h
@@ -1,1 +1,22 @@
 #pragma once
+#include <cuda_runtime.h>
+
+#include <ATen/cuda/detail/PhiloxCudaStateRaw.cuh>
+#include <ATen/cuda/detail/UnpackRaw.cuh>
+#include <memory>
+
+#include "logger.h"
+
+#define C10_CUDA_CHECK(EXPR) CHECK_CUDART(EXPR)
+
+#define C10_CUDA_KERNEL_LAUNCH_CHECK() C10_CUDA_CHECK(cudaGetLastError())
+
+namespace at::cuda {
+std::unique_ptr<cudaDeviceProp> getCurrentDeviceProperties() {
+  int device;
+  CHECK_CUDART(cudaGetDevice(&device));
+  cudaDeviceProp prop;
+  CHECK_CUDART(cudaGetDeviceProperties_v2(&prop, device));
+  return std::make_unique<cudaDeviceProp>(prop);
+}
+}  // namespace at::cuda
---
+++ src/flash_attention/fake_torch/ATen/cuda/CUDAGeneratorImpl.h
@@ -0,0 +1,5 @@
+// https://github.com/pytorch/pytorch/blob/main/aten/src/ATen/cuda/PhiloxCudaState.h
+#pragma once
+#include <ATen/cuda/PhiloxCudaState.h>
+
+#include <cstdio>
