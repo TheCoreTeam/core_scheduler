1ab27c02d8b0fa41358d120467820d89f540372e
Jie Ren <jieren9806@gmail.com>
2024-05-09 15:45:48 +0300

    fix(test): implement flash implementation

--- test/test_flash_attention.cpp
+++ test/test_flash_attention.cpp
@@ -2,6 +2,7 @@
 #include <cublas_v2.h>
 #include <cuda_runtime.h>
 #include <gtest/gtest.h>
+#include <torch/nn/functional.h>
 
 #include <Eigen/Dense>
 
@@ -48,11 +49,40 @@ cublasComputeType_t toCublasComputeType() {
 }  // namespace
 
 namespace {
-template <typename T>
+template <typename Dtype>
 void TestForwardT(const dllm::ContextCompute &context) {
-  auto x = at::rand({5, 5});
-  auto y = at::rand({5, 5});
-  auto z = x + y;
+  auto B = 4, T = 1024, n_head = 32, n_embd = 1024;
+  auto q = at::randn({B, T, n_embd}, at::TensorOptions{}
+                                         .dtype(c10::ScalarType::Half)
+                                         .device(c10::DeviceType::CUDA));
+  auto k = at::randn({B, T, n_embd}, at::TensorOptions{}
+                                         .dtype(c10::ScalarType::Half)
+                                         .device(c10::DeviceType::CUDA));
+  auto v = at::randn({B, T, n_embd}, at::TensorOptions{}
+                                         .dtype(c10::ScalarType::Half)
+                                         .device(c10::DeviceType::CUDA));
+  k = k.view({B, T, n_head, n_embd / n_head});  // (B, T, nh, hs)
+  q = q.view({B, T, n_head, n_embd / n_head});  // (B, T, nh, hs)
+  v = v.view({B, T, n_head, n_embd / n_head});  // (B, T, nh, hs)
+  auto scale = 1.0 / std::sqrt(static_cast<double>(k.size(-1)));
+  q = q.transpose(1, 2);
+  k = k.transpose(1, 2);
+  v = v.transpose(1, 2);
+  auto mask =
+      at::tril(at::ones({1, 1, T, T}, at::TensorOptions{}
+                                          .dtype(c10::ScalarType::Half)
+                                          .device(c10::DeviceType::CUDA)));
+  auto att = at::matmul(q, k.transpose(-1, -1)) * scale;
+  att = att.masked_fill(
+      mask.index({"...", "...",
+                  torch::indexing::Slice(torch::indexing::None, T),
+                  torch::indexing::Slice(torch::indexing::None, T)}) == 0,
+      -std::numeric_limits<float>::infinity());
+  att = torch::nn::functional::softmax(att, -1);
+  att = torch::nn::functional::dropout(
+      att, torch::nn::functional::DropoutFuncOptions{}.p(0.0));
+  auto y_attn = at::matmul(att, v);
+  y_attn = y_attn.transpose(1, 2);
 }
 }  // namespace
 
