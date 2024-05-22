c684b2c047ca522bd840b874d5b645f9524d4a98
Jie Ren <jieren9806@gmail.com>
2024-05-22 21:49:46 +0300

    feat(memory): use require restrict dimension

--- include/memory/allocate.h
+++ include/memory/allocate.h
@@ -5,8 +5,14 @@
 
 namespace dllm::memory {
 template <int N>
-TaskCompute allocateRowMajor(
-    std::shared_ptr<Tensor<N>> &p,
-    const typename repeat_type<TensorIndexType, N, cute::Shape>::type &shape,
-    const Dtype &dtype, const DeviceType &deviceType);
+  requires(N >= 1 && N <= 4)
+TaskCompute allocate(std::shared_ptr<Tensor<N>> &p,
+                     const typename Tensor<N>::Layout &layout,
+                     const Dtype &dtype, const DeviceType &deviceType);
+
+template <int N>
+  requires(N >= 1 && N <= 4)
+TaskCompute allocateRowMajor(std::shared_ptr<Tensor<N>> &p,
+                             const typename Tensor<N>::Shape &shape,
+                             const Dtype &dtype, const DeviceType &deviceType);
 }  // namespace dllm::memory
--- src/memory/allocate.cpp
+++ src/memory/allocate.cpp
@@ -2,10 +2,10 @@
 
 namespace dllm::memory {
 template <int N>
-TaskCompute allocateRowMajor(
-    std::shared_ptr<Tensor<N>> &p,
-    const typename repeat_type<TensorIndexType, N, cute::Shape>::type &shape,
-    const Dtype &dtype, const DeviceType &deviceType) {
+  requires(N >= 1 && N <= 4)
+TaskCompute allocateRowMajor(std::shared_ptr<Tensor<N>> &p,
+                             const typename Tensor<N>::Shape &shape,
+                             const Dtype &dtype, const DeviceType &deviceType) {
   const auto layout = cute::make_layout(shape, cute::GenRowMajor{});
   p = std::make_shared<Tensor<N>>(nullptr, layout, dtype, deviceType);
   const std::size_t sizeInByte = toByte(dtype) * cute::cosize(layout);
@@ -28,20 +28,20 @@ TaskCompute allocateRowMajor(
   return task;
 }
 
-template TaskCompute allocateRowMajor<1>(
-    std::shared_ptr<Tensor<1>> &p,
-    const typename repeat_type<TensorIndexType, 1, cute::Shape>::type &shape,
-    const Dtype &dtype, const DeviceType &deviceType);
-template TaskCompute allocateRowMajor<2>(
-    std::shared_ptr<Tensor<2>> &p,
-    const typename repeat_type<TensorIndexType, 2, cute::Shape>::type &shape,
-    const Dtype &dtype, const DeviceType &deviceType);
-template TaskCompute allocateRowMajor<3>(
-    std::shared_ptr<Tensor<3>> &p,
-    const typename repeat_type<TensorIndexType, 3, cute::Shape>::type &shape,
-    const Dtype &dtype, const DeviceType &deviceType);
-template TaskCompute allocateRowMajor<4>(
-    std::shared_ptr<Tensor<4>> &p,
-    const typename repeat_type<TensorIndexType, 4, cute::Shape>::type &shape,
-    const Dtype &dtype, const DeviceType &deviceType);
+template TaskCompute allocateRowMajor<1>(std::shared_ptr<Tensor<1>> &p,
+                                         const Tensor<1>::Shape &shape,
+                                         const Dtype &dtype,
+                                         const DeviceType &deviceType);
+template TaskCompute allocateRowMajor<2>(std::shared_ptr<Tensor<2>> &p,
+                                         const Tensor<2>::Shape &shape,
+                                         const Dtype &dtype,
+                                         const DeviceType &deviceType);
+template TaskCompute allocateRowMajor<3>(std::shared_ptr<Tensor<3>> &p,
+                                         const Tensor<3>::Shape &shape,
+                                         const Dtype &dtype,
+                                         const DeviceType &deviceType);
+template TaskCompute allocateRowMajor<4>(std::shared_ptr<Tensor<4>> &p,
+                                         const Tensor<4>::Shape &shape,
+                                         const Dtype &dtype,
+                                         const DeviceType &deviceType);
 }  // namespace dllm::memory
