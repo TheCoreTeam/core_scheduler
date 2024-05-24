dc1d92c76f8fc73b0093c4be2271172eb0773866
Jie Ren <jieren9806@gmail.com>
2024-05-25 00:32:33 +0300

    fix(compile): fix CXX11 ABI issue by using libtorch

--- test/main.cpp
+++ test/main.cpp
@@ -1,19 +1,6 @@
-// #include <gtest/gtest.h>
-//
-// int main(int argc, char **argv) {
-//   ::testing::InitGoogleTest(&argc, argv);
-//   return RUN_ALL_TESTS();
-// }
+#include <gtest/gtest.h>
 
-#include <catch2/catch_session.hpp>
-
-int main(int argc, char** argv) {
-  Catch::Session session;  // There must be exactly one instance
-
-  // Let Catch2 (using Clara) parse the command line
-  int returnCode = session.applyCommandLine(argc, argv);
-  if (returnCode != 0)  // Indicates a command line error
-    return returnCode;
-
-  return session.run();
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
