c9d0ca0ec117fd94328c04ac12e3f3b8a8482035
Hello_World <jieren9806@gmail.com>
2024-05-29 15:58:44 +0300

    Dev remove flash (#12)
    * feat(flash_attention): stop using flash attention!
    
    * feat(flash_attention): stop using flash attention!
    
    * feat(flash_attention): enable pcg_cpp
    
    * feat(flash_attention): disable dllm_flash_attention_plugin

--- src/flash_attention/fake_torch/ATen/cuda/CUDAGeneratorImpl.h
+++
@@ -1,6 +0,0 @@
-// https://github.com/pytorch/pytorch/blob/main/aten/src/ATen/cuda/PhiloxCudaState.h
-#pragma once
-#include <ATen/cuda/PhiloxCudaState.h>
-
-#include <cstdio>
-#include <functional>
